dd10c1aaa29da22936f91b0e9809f59a57405fbc
SakshiJoshi-2 <70576387+SakshiJoshi-2@users.noreply.github.com>
2020-09-04 16:29:06 +0530

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,16 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
+     
       <h1 class="title">
         new
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h4> New Repository</h4>
+      
     </div>
   </div>
 </template>
-
+git
 <script>
 export default {}
 </script>
