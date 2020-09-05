6d5620776f8d97f6e421505e06f5a60677565c6c
SakshiJoshi-2 <70576387+SakshiJoshi-2@users.noreply.github.com>
2020-09-05 07:10:54 +0530

    2

---
+++ components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">SMS</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">
+              Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/pages/navitems/about.vue">About</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="/pages/navitems/features.vue">Features</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="/pages/navitems/pricing.vue">Pricing</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="/pages/navitems/login.vue">Login</a>
+          </li>
+         
+          
+        </ul>
+       
+      </div>
+    </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style >
+.navbar{
+
+  z-index: 2;
+ 
+}
+</style>
---
+++ pages/admin/createrole.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>create role
+        
+      </h1>
+      
+    
+      
+    </div>
+</template>
+
+<script>
+    export default {
+     layout:'adminlayout'
+  }
+        
+    
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
