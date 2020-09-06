912a55255971e3caf42da312c4d383fb46b199ee
jainpriyanshe <jainpriya9595@gmail.com>
2020-09-06 14:18:50 +0530

    commit

--- pages/teacher/mysalary.vue
+++ pages/teacher/mysalary.vue
@@ -1,8 +1,19 @@
 <template>
-    <div>
-<h1>My Salary</h1>
+<div id="mysalary">
+    <h1>My Salary</h1>
     </div>
-</template>
+    </template>
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    year
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">2018</a>
+    <a class="dropdown-item" href="#">2019</a>
+    <a class="dropdown-item" href="#">2020</a>
+  </div>  
+  </div>
+  
 
 <script>
     export default {
