316169a293b2a28de4d70a70b54f5f42920ea122
Sakshi Joshi <sakshijoshi2626@gmail.com>
2020-11-11 13:39:20 +0530

    k

--- components/SidebarStudent.vue
+++ components/SidebarStudent.vue
@@ -68,27 +68,4 @@ export default {
   margin-right: 40px;
   z-index: 999;
 }
-.sidenav {
-  height: 100%;
-  width: 160px;
-  position: fixed;
-  z-index: 1;
-  top: 0px;
-  left: 0;
-  background-color: #212529;
-  overflow-x: hidden;
-  padding-top: 20px;
-  margin-top: 30px;
-}
-.sidenav a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 20px;
-  color: #818181;
-  display: block;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
 </style>
